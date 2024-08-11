6c09e4f15b1f9a43e68cf575c6bdc4d0b5dcb654
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-11 18:31:15 -0300

    Variables y elementos del DOM definidos

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let puntosUsuario = 0;
+let puntosPC = 0;
+
+let instrucciones = document.querySelector('#instrucciones');
+let contenedorPuntosUsuario = document.querySelector('#puntos-usuario');
+let contenedorPuntosPC = document.querySelector('#puntos-computadora');
+let mensaje = document.querySelector('#mensaje');
+let contenedorGanaPunto = document.querySelector('#gana-punto');
+let eligeTuArma = document.querySelector('#elige-tu-arma');
+
+let contenedorEleccionUsuario = document.querySelector('#eleccion-usuario');
+let contenedorEleccionPC = document.querySelector('#eleccion-computadora');
